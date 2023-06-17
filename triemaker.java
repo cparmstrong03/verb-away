54dde7b9db2b599debf73929b63414e8fef3c32a
Connor Armstrong <cparmstrong5@berkeley.edu>
2023-06-17 16:46:46 -0700

    trying to test

--- triemaker.java
+++ triemaker.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 import java.io.*;
 import tripackage.Trie;
 
-public class ReadWords {
+public class triemaker {
   public static void main(String[] args) throws IOException {
 
-    File file = new File("words.txt");
+    File file = new File("./words.txt");
     Scanner input = new Scanner(file);
+    File outFile = new File("./tri.io");
 
     Trie tri = new Trie();
 
     while (input.hasNext()) {
       String word = input.next();
+      System.out.print(word);
       tri.insert(word);
     }
 
--- tripackage/Trie.java
+++ tripackage/Trie.java
@@ -66,40 +66,5 @@ public class Trie implements Serializable {
     }
 
     // Driver
-    public static void main(String args[]) {
-        // Input keys (use only 'a' through 'z' and lower case)
-        String keys[] = { "the", "a", "there", "answer", "any",
-                "by", "bye", "their" };
 
-        String output[] = { "Not present in trie", "Present in trie" };
-
-        root = new TrieNode();
-
-        // Construct trie
-        int i;
-        for (i = 0; i < keys.length; i++)
-            insert(keys[i]);
-
-        // Search for different keys
-        if (search("the") == true)
-            System.out.println("the --- " + output[1]);
-        else
-            System.out.println("the --- " + output[0]);
-
-        if (search("these") == true)
-            System.out.println("these --- " + output[1]);
-        else
-            System.out.println("these --- " + output[0]);
-
-        if (search("their") == true)
-            System.out.println("their --- " + output[1]);
-        else
-            System.out.println("their --- " + output[0]);
-
-        if (search("thaw") == true)
-            System.out.println("thaw --- " + output[1]);
-        else
-            System.out.println("thaw --- " + output[0]);
-
-    }
 }
